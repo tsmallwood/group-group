3b2369f4e08d87afc8760a4c82adecb41af74e5e
amilhousecunningham <92339988+amilhousecunningham@users.noreply.github.com>
2021-12-08 14:13:29 -0500

    updated login page js files on main branch

--- Login Page/index.js
+++ Login Page/index.js
@@ -3,23 +3,23 @@ const loginManager = new loginController();
 
 loginManager.storeLogin();
 
+let usernameInput = document.getElementById('username');
+let passwordInput = document.getElementById('password');
 
-let gearRows = document.getElementById('gearRows');
-let addGearBtn = document.getElementById('addGearButton');
+// I am not quite sure what to do for the eventListener. I will update it soon.
 
-addGearBtn.addEventListener("click", function(event){
-    event.preventDefault();
+// submitLoginBtn.addEventListener("click", function(event){
+//     event.preventDefault();
 
-    let gearURL = document.getElementById("gearURL");
-    let gearURL = document.getElementById("gearType");
-    let gearURL = document.getElementById("gearPrice");
-    let gearURL = document.getElementById("gearUsedFor");
+//     let gearURL = document.getElementById("gearURL");
+//     let gearURL = document.getElementById("gearType");
 
-    gearManager.addGear(gearURL.value, gearType.value, gearPrice.value, gearUsedFor.value);
 
-    gearURL.value = "";
-    gearType.value = "";
-    gearPrice.value = "";
-    gearUsedFor.value = "";
+//     gearManager.addGear(gearURL.value, gearType.value, gearPrice.value, gearUsedFor.value);
 
-})
+//     gearURL.value = "";
+//     gearType.value = "";
+//     gearPrice.value = "";
+//     gearUsedFor.value = "";
+
+// })
